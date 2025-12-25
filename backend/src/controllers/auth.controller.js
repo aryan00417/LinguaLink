1603350408e2db28f1a8960f36db7fa221854867
ARYAN <aryan.00417@gmail.com>
2025-12-25 13:02:45 +0530

    initial backend setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req,res)=>{
+  res.send("signUp")
+}
+
+export const login = async  (req,res)=>{
+  res.send("login")
+}
+
+export const logout =   (req,res)=>{
+  res.send("logout")
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import authRoute from "./routes/auth.route.js"
 
 dotenv.config();
 
 const PORT = process.env.PORT
 const app = express()
 
-app.get("/api/auth/signup",(req,res)=>{
-  res.send("Signup route")
-})
-
-app.get("/api/auth/login",(req,res)=>{
-  res.send("login route")
-})
-
-app.get("/api/auth/logout",(req,res)=>{
-  res.send("Logout route")
-})
+app.use("/api/auth",authRoute)
 
 app.listen(PORT,()=>{
   console.log(`the server is running on port ${PORT}`)
