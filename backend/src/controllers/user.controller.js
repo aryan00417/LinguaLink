231dc0abf78db43c8b586f0c1a40a2053f07d030
ARYAN <aryan.00417@gmail.com>
2025-12-26 19:07:08 +0530

    get friends api route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,28 +1,36 @@
-
 import User from "../models/User.js";
 
-export const getRecommendedUsers = async(req,res)=>{
+export const getRecommendedUsers = async (req, res) => {
   try {
-    
     const currentUserId = req.user.id;
     const currentUser = req.user;
 
     const recommendedUsers = await User.find({
       $and: [
-        {_id: {$ne:currentUserId}}, //exclude current user
-        {_id: {$nin: currentUser.friends}}, //exclude current users friends
-        {isOnboarded: true}
-      ]
-    })
-    res.status(200).json(recommendedUsers)
+        { _id: { $ne: currentUserId } }, //exclude current user
+        { _id: { $nin: currentUser.friends } }, //exclude current users friends
+        { isOnboarded: true },
+      ],
+    });
+    res.status(200).json(recommendedUsers);
   } catch (error) {
-    console.log("error in geting recommendedUsers")
-    res.status(500).json({message: "Internal server error"})
+    console.log("error in geting recommendedUsers");
+    res.status(500).json({ message: "Internal server error" });
   }
+};
 
-}
+export const getMyFriends = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")
+      .populate(
+        "friends",
+        "fullName profilePic nativeLanguage learningLanguage"
+      );
 
-export const getMyFriends = async(req,res)=>{
-  
-  
-}
+    res.status(200).json(user.friends);
+  } catch (error) {
+    console.log("Error in getMyFriends:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
