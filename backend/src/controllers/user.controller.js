04d9b1fe6088b395baa469ceabe7b8dbcaeec5ab
ARYAN <aryan.00417@gmail.com>
2025-12-26 20:00:18 +0530

    accepting friend request api route

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -78,3 +78,37 @@ export const sendFriendRequest = async(req,res)=>{
     return res.status(500).json({message: "Internal server error"})
   }
 }
+
+export const acceptFriendRequest = async(req,res)=>{  
+  try {
+    const {id:requestId} = req.params
+    const friendRequest = await FriendRequest.findById(requestId);
+
+    if(!friendRequest){
+      return res.status(404).json({message: "Friend Request not found"})
+    }
+
+    //verify if the current user is recepient
+    if(friendRequest.recipient.toString() !== req.user.id.toString()){
+      return res.status(403).json({message: "You are not authorized to accept this request"})
+    }
+
+    friendRequest.status = "accepted";
+    await friendRequest.save();
+
+    //add each user to others friends request
+    // $addToSet adds elements to array if they do not exsist in that array
+    await User.findByIdAndUpdate(friendRequest.sender,{
+      $addToSet: {friends: friendRequest.recipient}
+    })
+     await User.findByIdAndUpdate(friendRequest.recipient,{
+      $addToSet: {friends: friendRequest.sender}
+    })
+
+    res.status(200).json({message: "Friend request accepted"})
+
+  } catch (error) {
+    console.log("Error in accepting friend request ",error.message)
+    return res.status(500).json({message: "Internal server error"})
+  }
+}
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
-import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js"
+import { acceptFriendRequest, getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controller.js"
 
 const router = express.Router()
 
@@ -10,5 +10,6 @@ router.use(protectRoute)
 router.get("/",getRecommendedUsers)
 router.get("/friends",getMyFriends)
 router.post("/friend-request/:id",sendFriendRequest)
+router.post("/friend-request/:id/accept",acceptFriendRequest)
 
 export default router
