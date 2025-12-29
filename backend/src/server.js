2db4072288f7ee2eb6ae553e72072b45aafd3b19
ARYAN <aryan.00417@gmail.com>
2025-12-29 18:07:13 +0530

    setting up axios ,cors and tenstack query

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,12 +5,17 @@ import usersRoute from "./routes/user.route.js"
 import chatRoute from "./routes/chat.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT
 
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true //allow frontend to send cookies
+}))
 
 app.use(express.json())
 app.use(cookieParser());
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,26 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
-import toast, {Toaster} from "react-hot-toast";
+import  {Toaster} from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { axiosInstance } from "./lib/axios.js";
 
 export default function App() {
+
+ const {data,isLoading,error} = useQuery({queryKey:["todos"],
+
+  queryFn: async()=>{
+    const res = await axiosInstance.get("/auth/me")
+    return res.data;
+  },
+  retry: false
+ })
+ console.log({data})
+ 
+
   return (
     <div className=" h-screen text-5xl "data-theme="coffee">
-
-      <button onClick={()=> toast.success("Hey there")}>toast</button>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
