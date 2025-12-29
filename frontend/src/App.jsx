17e06cb8876ed5a197bf8a605c84050af570bfa3
ARYAN <aryan.00417@gmail.com>
2025-12-29 16:33:21 +0530

    setting up react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,27 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+
 export default function App() {
   return (
-    
-    <div></div>
+    <div className=" h-screen text-5xl "data-theme="coffee">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+
+        
+      </Routes>
+
+    </div>
   );
 }
---
+++ frontend/src/pages/OnboardingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OnboardingPage = () => {
+  return (
+    <div>Onboarding</div>
+  )
+}
+
+export default OnboardingPage
